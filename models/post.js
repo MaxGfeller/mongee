00b8ff641c546ef614167ee48b30fd19c26197e1
Max Gfeller <max.gfeller@gmail.com>
2011-02-24 14:42:24 +0100

    save coments directly into post object

--- models/post.js
+++ models/post.js
@@ -3,19 +3,20 @@ var mongoose		= require("../lib/mongoose/lib/mongoose");
 var Schema			= mongoose.Schema;
 var ObjectId		= Schema.ObjectId;
 
+var Comment 		= mongoose.model("Comment");
 
 var Post = new Schema({
 	user				: ObjectId,
 	on_wall			: [ObjectId],
 	content			: String, 
 	link		 		: String,
-	comment			: [ObjectId],
+	comment			: [Comment],
 	created_at	: Date
 });
 
 Post.pre("save", function(next){
 	this.set("created_at", new Date());
 	next();
-}):
+});
 
 exports.Post = Post;
