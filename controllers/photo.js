e9ce48e1d07462ef66e25836373a862a93d94ece
Max Gfeller <max.gfeller@gmail.com>
2011-03-17 17:10:12 +0100

    added ability to upload photos directly into albums

--- controllers/photo.js
+++ controllers/photo.js
@@ -2,7 +2,6 @@ var mongoose		= require("mongoose");
 var formidable  = require("formidable");
 var util 				= require("util");
 var fs					= require("fs");
-//var hash				= require("hash");
 
 var User = mongoose.model("User");
 var Photo = mongoose.model("Photo");
@@ -12,14 +11,14 @@ module.exports = {
 	
 	mapping: {
 		"create" : {
-			"url":"/photos",
+			"url":"/photos/:album?",
 			"method":"post",
 			"description":"upload one or multiple images",
 			"auth":true
 		}
 	}, 
 	
-	// PUT /photos
+	// PUT /photos/:album?
 	create: function(req, res) {
 		auth.handle_authorized_request(req, res, function(req, res, user){
 			var form = new formidable.IncomingForm();
@@ -27,15 +26,19 @@ module.exports = {
       var fields = [];
 
 			var path = "public/photos/" + user._id.toString();
+			if(req.params.album) {
+				path += "/" + req.params.album;
+			}
 
-			console.log("path: " + path);
+			
 			fs.mkdir(path, 0777);
     	form.uploadDir = path;
 	    form.parse(req);
 	    form.addListener("file", function(field, file){
 	    	var ext = /[^.]+$/.exec(file.name);
-	    	fs.rename(file.path, path + "/" + file.name);
-	    	res.send("ok", 200);
+	    	var newFileName = "photo_" + user._id.toString() + "_" + new Date().getTime() + "." + ext;
+	    	fs.rename(file.path, path + "/" + newFileName);
+	    	res.send(newFileName, 200);
 	    });
 		});
 	}
